2f925e0c0211b25fdf04f9030c7de9dd89c437a2
Campbell Rex <campbellrex@outlook.com>
2023-01-10 14:19:42 -0800

    Added counting output

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, GitHub!");
+
+Console.WriteLine("Below is a filler loop counting to 10.");
+Console.WriteLine("The loop serves no purpose.");
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Number: {i}");
+}
